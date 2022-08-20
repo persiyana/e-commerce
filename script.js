b544da4c50628d37b4d08d255927a0eedc7008ef
Persiyana <persiyanageorgieva@gmail.com>
2022-08-20 12:45:38 +0300

    Integrating the app with Asos API

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'b2cf30ec30msh8bfb7e980100998p1da74fjsnc1233039b101',
+        'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
+    }
+};
+
+fetch('https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=4209&limit=48&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US', options)
+    .then(response => response.json())
+    .then(response => {
+        document.getElementById('categoryName').innerHTML += response.categoryName;
+        const indexes = [];
+        while (indexes.length < 20) {
+            const index = Math.floor(Math.random() * response.products.length);
+            if (!indexes.includes(index)) {
+                indexes.push(index);
+         }
+        }
+        for (const index of indexes) {
+            const div = document.createElement('li');
+            div.classList.add('product'+index);
+            const prod =response.products[index];
+            
+            div.innerHTML += `<img src="http://${prod.imageUrl}" /><br/>`;
+            div.innerHTML += `<p>${prod.name}</p><br/>`;
+            div.innerHTML += `<h4>${prod.price.current.text}</h4><br/><button type="button">Buy</button>`;
+            document.getElementById('list-of-products').appendChild(div);
+        }
+    })
+    .catch(err => console.error(err));
